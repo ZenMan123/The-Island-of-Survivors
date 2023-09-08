d645b52afd1108f085523db00c9fe61a03c8fa86
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-09-08 09:27:16 +0300

    Fixed SFMLSharedTargets.cmake

--- libraries/SFML/SFMLSharedTargets.cmake
+++ libraries/SFML/SFMLSharedTargets.cmake
@@ -54,21 +54,14 @@ unset(_cmake_expected_targets)
 add_library(sfml-system SHARED IMPORTED)
 
 set_target_properties(sfml-system PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
-)
-
-# Create imported target sfml-main
-add_library(sfml-main STATIC IMPORTED)
-
-set_target_properties(sfml-main PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
 )
 
 # Create imported target sfml-window
 add_library(sfml-window SHARED IMPORTED)
 
 set_target_properties(sfml-window PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
   INTERFACE_LINK_LIBRARIES "sfml-system"
 )
 
@@ -83,7 +76,7 @@ set_target_properties(OpenGL PROPERTIES
 add_library(sfml-network SHARED IMPORTED)
 
 set_target_properties(sfml-network PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
   INTERFACE_LINK_LIBRARIES "sfml-system"
 )
 
@@ -91,93 +84,51 @@ set_target_properties(sfml-network PROPERTIES
 add_library(sfml-graphics SHARED IMPORTED)
 
 set_target_properties(sfml-graphics PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
   INTERFACE_LINK_LIBRARIES "sfml-window"
-)
-
-# Create imported target Freetype
-add_library(Freetype INTERFACE IMPORTED)
-
-set_target_properties(Freetype PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/headers/freetype2"
-  INTERFACE_LINK_LIBRARIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/freetype.lib"
-)
-
-# Create imported target OpenAL
-add_library(OpenAL INTERFACE IMPORTED)
-
-set_target_properties(OpenAL PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/headers/AL"
-  INTERFACE_LINK_LIBRARIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/openal32.lib"
-)
-
-# Create imported target VORBIS
-add_library(VORBIS INTERFACE IMPORTED)
-
-set_target_properties(VORBIS PROPERTIES
-  INTERFACE_COMPILE_DEFINITIONS "OV_EXCLUDE_STATIC_CALLBACKS"
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/headers;C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/headers"
-  INTERFACE_LINK_LIBRARIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/vorbisenc.lib;C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/vorbisfile.lib;C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/vorbis.lib;C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/ogg.lib"
-)
-
-# Create imported target FLAC
-add_library(FLAC INTERFACE IMPORTED)
-
-set_target_properties(FLAC PROPERTIES
-  INTERFACE_COMPILE_DEFINITIONS "FLAC__NO_DLL"
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/headers"
-  INTERFACE_LINK_LIBRARIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/extlibs/libs-msvc-universal/x64/flac.lib"
 )
 
 # Create imported target sfml-audio
 add_library(sfml-audio SHARED IMPORTED)
 
 set_target_properties(sfml-audio PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/include"
+  INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
   INTERFACE_LINK_LIBRARIES "sfml-system"
 )
 
 # Import target "sfml-system" for configuration "Debug"
 set_property(TARGET sfml-system APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(sfml-system PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-system-d.a"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-system-d-2.dll"
-  )
-
-# Import target "sfml-main" for configuration "Debug"
-set_property(TARGET sfml-main APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
-set_target_properties(sfml-main PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-main-d.lib"
-  )
+  IMPORTED_IMPLIB_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/sfml-system-d.a"
+)
 
 # Import target "sfml-window" for configuration "Debug"
 set_property(TARGET sfml-window APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(sfml-window PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-window-d.a"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-window-d-2.dll"
-  )
+  IMPORTED_IMPLIB_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/sfml-window-d.a"
+)
 
 # Import target "sfml-network" for configuration "Debug"
 set_property(TARGET sfml-network APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(sfml-network PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-network-d.a"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-network-d-2.dll"
-  )
+  IMPORTED_IMPLIB_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/sfml-network-d.a"
+)
 
 # Import target "sfml-graphics" for configuration "Debug"
 set_property(TARGET sfml-graphics APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(sfml-graphics PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-graphics-d.a"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-graphics-d-2.dll"
-  )
+  IMPORTED_IMPLIB_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/sfml-graphics-d.a"
+)
 
 # Import target "sfml-audio" for configuration "Debug"
 set_property(TARGET sfml-audio APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(sfml-audio PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-audio-d.a"
-  IMPORTED_LOCATION_DEBUG "C:/Users/ga_pi/Desktop/GrigoriyPA/projects_C++/LIB/Build/SFML-2.6.0/build/lib/sfml-audio-d-2.dll"
-  )
+  IMPORTED_IMPLIB_DEBUG "${CMAKE_CURRENT_LIST_DIR}/lib/sfml-audio-d.a"
+)
+
+# Set DLL packages
+set(SFML_DLL_PACKAGES_PATH "${CMAKE_CURRENT_LIST_DIR}/dll")
+set(SFML_DLL_PACKAGES sfml-system-d-2.dll sfml-window-d-2.dll sfml-graphics-d-2.dll sfml-network-d-2.dll sfml-audio-d-2.dll)
 
 # This file does not depend on other imported targets which have
 # been exported from the same project but in a separate export set.
