8339dfa316fd5bc08974be26b8a326f006e8aa71
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-09-23 01:50:45 +0300

    Added PhysicObject

--- src/physics/physic_object.cpp
+++ src/physics/physic_object.cpp
@@ -1,1 +1,7 @@
 #include "physic_object.hpp"
+
+
+// Constructors
+PhysicObject::PhysicObject(const Vec2& position) noexcept
+    : position(position)
+{}
--- src/physics/physic_object.hpp
+++ src/physics/physic_object.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+#include <common/math/vec2.hpp>
+
+
+// Common interface for physic 
 class PhysicObject {
 public:
+    Vec2 speed = Vec2(0.0);
 
+    Vec2 position;
+
+    // Constructors
+    PhysicObject(const Vec2& position) noexcept;
 };
