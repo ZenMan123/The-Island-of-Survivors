af6d3f09d8fc3d8b7a81ec6be1b58726a935c7d4
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-10-09 00:52:33 +0300

    Added update

--- src/physics/physical_objects/physical_object.cpp
+++ src/physics/physical_objects/physical_object.cpp
@@ -5,3 +5,12 @@
 PhysicalObject::PhysicalObject(const Vec2& position) noexcept
     : position(position)
 {}
+
+// Common methods
+void PhysicalObject::intersect(PhysicalObject::Ptr other, std::vector<Intersection>& intersections) const {
+    border->intersect(other->border, intersections);
+}
+
+void PhysicalObject::update(double time) noexcept {
+    position += speed * time;
+}
--- src/physics/physical_objects/physical_object.hpp
+++ src/physics/physical_objects/physical_object.hpp
@@ -6,10 +6,18 @@
 // Common interface for physical objects 
 class PhysicalObject {
 public:
+    using Ptr = std::shared_ptr<PhysicalObject>;
+
     Vec2 speed = Vec2(0.0);
 
     Vec2 position;
+    ObjectBorder::Ptr border;
 
     // Constructors
     PhysicalObject(const Vec2& position) noexcept;
+
+    // Common methods
+    void intersect(PhysicalObject::Ptr other, std::vector<Intersection>& intersections) const;
+
+    void update(double time) noexcept;
 };
