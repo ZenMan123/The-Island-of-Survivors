e170b56f54353fce942d84d1cd45758057a8c6fa
Григорий Писаренко <ga_pisarenko@mail.ru>
2023-10-18 01:28:55 +0300

    Added GameObject

--- src/graphics/game_objects/game_object.cpp
+++ src/graphics/game_objects/game_object.cpp
@@ -1,1 +1,35 @@
+#include "game_object.hpp"
 
+#include <graphics/graphics_context.hpp>
+#include <physics/physical_context.hpp>
+
+
+//// GameObject
+
+// Constructors
+GameObject::GameObject(DrawableObject::Ptr drawable_object, PhysicalObject::Ptr physical_object)
+    : drawable_object_(drawable_object)
+    , physical_object_(physical_object)
+{
+    insert_into_context();
+}
+
+// Private functions
+void GameObject::insert_into_context() {
+    if (drawable_object_) {
+        drawable_object_id_ = GraphicsContext::GetInstance()->drawable_objects.insert(drawable_object_);
+    }
+    if (physical_object_) {
+        physical_object_id_ = PhysicalContext::GetInstance()->physical_objects.insert(physical_object_);
+    }
+}
+
+// Destructors
+GameObject::~GameObject() {
+    if (drawable_object_) {
+        GraphicsContext::GetInstance()->drawable_objects.erase(drawable_object_id_);
+    }
+    if (physical_object_) {
+        PhysicalContext::GetInstance()->physical_objects.erase(physical_object_id_);
+    }
+}
--- src/graphics/game_objects/game_object.hpp
+++ src/graphics/game_objects/game_object.hpp
@@ -5,12 +5,21 @@
 
 // Game object with drawable and physical parts
 class GameObject {
+    // Private functions
     void insert_into_context();
 
+    size_t drawable_object_id_;
+    size_t physical_object_id_;
+
+    DrawableObject::Ptr drawable_object_;
+    PhysicalObject::Ptr physical_object_;
+
 public:
-    DrawableObject::Ptr drawable_object;
-    PhysicalObject::Ptr physical_object;
+    using Ptr = std::shared_ptr<GameObject>;
 
     // Constructors
     GameObject(DrawableObject::Ptr drawable_object, PhysicalObject::Ptr physical_object);
+
+    // Destructors
+    ~GameObject();
 };
